527556604c1550d9b295c8476afe39ba0259f3d9
mrkekovich <mrkekovich.official@gmail.com>
2025-02-11 21:44:32 +0300

    test(ValidationContextTest)

---
+++ projects/core/kverify-core/src/commonTest/kotlin/io/github/kverify/core/context/ValidationContextTest.kt
@@ -0,0 +1,69 @@
+package io.github.kverify.core.context
+
+import io.github.kverify.core.model.Rule
+import io.github.kverify.core.violation.AnyViolation
+import io.github.kverify.core.violation.Violation
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+import kotlin.test.assertContains
+import kotlin.test.fail
+
+class ValidationContextTest :
+    FunSpec({
+
+        test("onFailure captures the violation") {
+            val violations = mutableListOf<Violation>()
+            val context = ValidationContext { violations.add(it) }
+
+            val testViolation = AnyViolation("Test Error")
+            context.onFailure(testViolation)
+
+            violations.size shouldBe 1
+            assertContains(violations, testViolation)
+        }
+
+        test("applyRules executes rules and records failures") {
+            val violations = mutableListOf<Violation>()
+            val context = ValidationContext { violations.add(it) }
+
+            val rule1 = DummyRule<Int>(true, AnyViolation("Rule 1 failed"))
+            val rule2 = DummyRule<Int>(false, AnyViolation("Rule 2 failed"))
+
+            42.applyRules(context, rule1, rule2)
+
+            violations.size shouldBe 1
+            assertContains(violations, rule2.violation)
+        }
+
+        test("validate triggers onFailure when condition is false") {
+            val violations = mutableListOf<Violation>()
+            val context = ValidationContext { violations.add(it) }
+            val testViolation = AnyViolation("Condition failed")
+
+            context.validate(false) { testViolation }
+
+            assertContains(violations, testViolation)
+        }
+
+        test("validate does not trigger onFailure when condition is true") {
+            val context = ValidationContext { fail("onFailure was triggered") }
+
+            context.validate(true) { AnyViolation("Condition failed") }
+        }
+
+        test("validate does not trigger violationGenerator lambda when condition is true") {
+            val context = ValidationContext { }
+
+            context.validate(true) { fail("violationGenerator was triggered") }
+        }
+    })
+
+// DummyRule class to simulate validation rules
+class DummyRule<T>(
+    val shouldFail: Boolean,
+    val violation: Violation,
+) : Rule<T> {
+    override fun ValidationContext.runValidation(value: T) {
+        if (shouldFail) onFailure(violation)
+    }
+}
