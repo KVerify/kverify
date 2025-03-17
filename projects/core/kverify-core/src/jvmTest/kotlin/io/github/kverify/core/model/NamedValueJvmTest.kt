3730a99cbf2347b137172ad331b0c841309d44de
tessoir <mrkekovich.official@gmail.com>
2025-03-17 10:50:26 +0300

    test(NamedValueJvmTest)

---
+++ projects/core/kverify-core/src/jvmTest/kotlin/io/github/kverify/core/model/NamedValueJvmTest.kt
@@ -0,0 +1,32 @@
+package io.github.kverify.core.model
+
+import io.github.kverify.core.exception.PropertyAccessException
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class NamedValueJvmTest :
+    FunSpec({
+        test("KProperty.toNamed") {
+            val testInstance = TestJvmClass(1)
+
+            val namedValue = testInstance::property.toNamed()
+            val thrownException =
+                shouldThrow<PropertyAccessException> {
+                    TestJvmClass::property.toNamed()
+                }
+
+            namedValue.name shouldBe "property"
+            namedValue.value shouldBe 1
+
+            thrownException.message shouldBe
+                """
+                Cannot access value of the property 'property' without an instance.
+                Hint: Use `instance::prop` instead of `Class::prop`
+                """.trimIndent()
+        }
+    })
+
+private class TestJvmClass(
+    val property: Int,
+)
