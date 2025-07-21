80bbc031fe1bf83d330b95d5095d4b002b2136f9
tessoir <mrkekovich.official@gmail.com>
2025-07-21 15:54:10 +0300

    fix(NamedViolationFactoryAdapter): Add "JvmInline" annotation

--- project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/violation/factory/NamedViolationFactory.kt
+++ project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/violation/factory/NamedViolationFactory.kt
@@ -2,13 +2,18 @@ package io.github.kverify.violation.factory
 
 import io.github.kverify.core.model.NamedValue
 import io.github.kverify.core.violation.Violation
+import kotlin.jvm.JvmInline
 
 public typealias NamedViolationFactory<T> = ViolationFactory<NamedValue<T>>
 
 @Suppress("NOTHING_TO_INLINE")
-public inline fun <T> ViolationFactory<T>.asNamedViolationFactory(): NamedViolationFactory<T> = NamedViolationFactoryAdapter(this)
+public inline fun <T> ViolationFactory<T>.asNamedViolationFactory(): NamedViolationFactory<T> =
+    NamedViolationFactoryAdapter(
+        baseFactory = this,
+    )
 
 @PublishedApi
+@JvmInline
 internal value class NamedViolationFactoryAdapter<T>(
     private val baseFactory: ViolationFactory<T>,
 ) : NamedViolationFactory<T> {
