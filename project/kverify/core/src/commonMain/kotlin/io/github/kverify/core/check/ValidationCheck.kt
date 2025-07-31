e84d7361db9b31520c4508b28c5790a1dc367266
tessoir <mrkekovich.official@gmail.com>
2025-07-31 14:29:57 +0300

    feat(InvertedValidationCheck)

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/check/ValidationCheck.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/check/ValidationCheck.kt
@@ -1,5 +1,17 @@
 package io.github.kverify.core.check
 
+import kotlin.jvm.JvmInline
+
 public fun interface ValidationCheck<in T> {
     public fun isValid(value: T): Boolean
+}
+
+@Suppress("NOTHING_TO_INLINE")
+public inline operator fun <T> ValidationCheck<T>.not(): ValidationCheck<T> = InvertedValidationCheck(this)
+
+@JvmInline
+public value class InvertedValidationCheck<in T>(
+    private val originalCheck: ValidationCheck<T>,
+) : ValidationCheck<T> {
+    override fun isValid(value: T): Boolean = !originalCheck.isValid(value)
 }
