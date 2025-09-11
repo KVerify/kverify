08a4f393ea2315ef8936e69d04943e885ab8a4d2
tessoir <mrkekovich.official@gmail.com>
2025-09-11 09:37:25 +0300

    feat(CollectionNotEmptyRule)

---
+++ project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/rule/set/collection/CollectionNotEmptyRule.kt
@@ -0,0 +1,15 @@
+package io.github.kverify.rule.set.collection
+
+import io.github.kverify.check.set.collection.CollectionNotEmptyCheck
+import io.github.kverify.core.check.ViolationFactory
+import io.github.kverify.core.rule.PredicateRule
+import io.github.kverify.core.rule.Rule
+import io.github.kverify.violation.factory.provider.CollectionViolationFactoryProvider
+
+public class CollectionNotEmptyRule<C : Collection<*>>(
+    public val violationFactory: ViolationFactory<C> =
+        CollectionViolationFactoryProvider.Default.notEmpty(),
+) : Rule<C> by PredicateRule(
+        validationCheck = CollectionNotEmptyCheck,
+        violationFactory = violationFactory,
+    )
