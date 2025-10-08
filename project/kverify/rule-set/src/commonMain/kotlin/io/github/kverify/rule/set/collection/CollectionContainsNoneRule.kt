9fadb7db26d7dda9dc308c453a006a6880e0470c
tessoir <mrkekovich.official@gmail.com>
2025-10-08 07:56:29 +0300

    refactor(CollectionContainsNoneRule): Remove unused disjoint function

--- project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/rule/set/collection/CollectionContainsNoneRule.kt
+++ project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/rule/set/collection/CollectionContainsNoneRule.kt
@@ -36,37 +36,3 @@ public inline fun <E, C : Collection<E>> CollectionContainsNoneRule(vararg eleme
 
     return CollectionContainsNoneRule(elements = elementsSet)
 }
-
-private fun disjoint(
-    c1: Collection<*>,
-    c2: Collection<*>,
-): Boolean {
-    if (c1.isEmpty() || c2.isEmpty()) return true
-
-    val iterate: Collection<*>
-    val contains: Collection<*>
-
-    when {
-        c1 is Set<*> -> {
-            iterate = c2
-            contains = c1
-        }
-
-        c2 is Set<*> -> {
-            iterate = c1
-            contains = c2
-        }
-
-        c1.size > c2.size -> {
-            iterate = c2
-            contains = c1
-        }
-
-        else -> {
-            iterate = c1
-            contains = c2
-        }
-    }
-
-    return iterate.none { it in contains }
-}
