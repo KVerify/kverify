402ad095406e11421ef4fe59e768fcc81efc8211
tessoir <mrkekovich.official@gmail.com>
2025-10-08 07:28:10 +0300

    refactor(ValidationException): Secondary constructors instead of factory functions

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/exception/ValidationException.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/exception/ValidationException.kt
@@ -1,8 +1,6 @@
 package io.github.kverify.core.exception
 
 import io.github.kverify.core.violation.Violation
-import io.github.kverify.core.violation.asViolationReason
-import kotlin.jvm.JvmName
 
 public open class ValidationException(
     message: String? = null,
@@ -11,56 +9,30 @@ public open class ValidationException(
 ) : Throwable(
         message = message,
         cause = cause,
+    ) {
+    public constructor(
+        violations: List<Violation>,
+        cause: Throwable? = null,
+    ) : this(
+        message =
+            if (violations.isEmpty()) {
+                "Validation failed"
+            } else {
+                "Validation failed:\n${
+                    violations.joinToString("\n") {
+                        "\t- ${it.reason}"
+                    }
+                }"
+            },
+        violations = violations,
+        cause = cause,
     )
 
-public fun ValidationException(
-    violations: List<Violation>,
-    cause: Throwable? = null,
-): ValidationException {
-    val message =
-        if (violations.isEmpty()) {
-            "Validation failed"
-        } else {
-            "Validation failed:\n${
-                violations.joinToString("\n") {
-                    "\t- ${it.reason}"
-                }
-            }"
-        }
-
-    return ValidationException(
-        violations = violations,
-        message = message,
+    public constructor(
+        vararg violations: Violation,
+        cause: Throwable? = null,
+    ) : this(
+        violations = violations.asList(),
         cause = cause,
     )
 }
-
-public fun ValidationException(
-    vararg violations: Violation,
-    cause: Throwable? = null,
-): ValidationException =
-    ValidationException(
-        violations.asList(),
-        cause,
-    )
-
-public fun ValidationException(
-    message: String? = null,
-    violationMessages: List<String> = emptyList(),
-    cause: Throwable? = null,
-): ValidationException =
-    ValidationException(
-        message,
-        violationMessages.map { it.asViolationReason() },
-        cause,
-    )
-
-@JvmName("ValidationExceptionFromMessages")
-public fun ValidationException(
-    violationMessages: List<String> = emptyList(),
-    cause: Throwable? = null,
-): ValidationException =
-    ValidationException(
-        violationMessages.map { it.asViolationReason() },
-        cause,
-    )
