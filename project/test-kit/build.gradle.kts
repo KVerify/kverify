f09faab7c9efccab948c12623a75ad46fdf8499a
tessoir <mrkekovich.official@gmail.com>
2025-10-11 09:47:47 +0300

    test: Remove kotest-property dependency

--- project/test-kit/build.gradle.kts
+++ project/test-kit/build.gradle.kts
@@ -10,7 +10,6 @@ kotlin {
     sourceSets {
         commonMain {
             dependencies {
-                api(libs.kotest.property)
                 api(kotlin("test"))
                 api(kotlin("test-common"))
                 api(kotlin("test-annotations-common"))
