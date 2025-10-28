0e88c3b571dc0dedc6ace21aa2a540fe6a37c196
tessoir <mrkekovich.official@gmail.com>
2025-10-28 18:51:55 +0300

    refactor(CollectionContainsAllCheck.isValid): Optimize the method
    Exit early on empty elements/value; convert the value to a hash set when the size threshold is crossed

--- project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/check/set/collection/CollectionContainsAllCheck.kt
+++ project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/check/set/collection/CollectionContainsAllCheck.kt
@@ -3,15 +3,35 @@ package io.github.kverify.check.set.collection
 import io.github.kverify.core.check.ValidationCheck
 import kotlin.jvm.JvmInline
 
+private const val HASH_SET_ALLOCATION_THRESHOLD = 64
+
 @JvmInline
 public value class CollectionContainsAllCheck<E, C : Collection<E>>(
     public val elements: Collection<E>,
 ) : ValidationCheck<C> {
-    override fun isValid(value: C): Boolean = value.containsAll(elements)
+    override fun isValid(value: C): Boolean =
+        when {
+            elements.isEmpty() -> true
+            value.isEmpty() -> false
+            value is Set<*> -> value.containsAll(elements)
+            else -> {
+                val n = value.size
+                val m = elements.size
+
+                val lookup =
+                    if (m > 1 && n > HASH_SET_ALLOCATION_THRESHOLD / m) {
+                        value.toSet()
+                    } else {
+                        value
+                    }
+
+                elements.all { it in lookup }
+            }
+        }
 }
 
 @Suppress("NOTHING_TO_INLINE")
 public inline fun <E, C : Collection<E>> CollectionContainsAllCheck(vararg elements: E): CollectionContainsAllCheck<E, C> =
     CollectionContainsAllCheck(
-        elements = elements.toSet(),
+        elements = setOf(elements = elements),
     )
