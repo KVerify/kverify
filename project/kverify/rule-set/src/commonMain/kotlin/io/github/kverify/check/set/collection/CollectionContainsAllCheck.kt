407d6d3b31baedca9b6508ed31224a8ca0ed6dbf
tessoir <mrkekovich.official@gmail.com>
2025-10-29 14:58:17 +0300

    refactor(CollectionContainsAllCheck.isValid): Remove hash set allocation

--- project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/check/set/collection/CollectionContainsAllCheck.kt
+++ project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/check/set/collection/CollectionContainsAllCheck.kt
@@ -3,8 +3,6 @@ package io.github.kverify.check.set.collection
 import io.github.kverify.core.check.ValidationCheck
 import kotlin.jvm.JvmInline
 
-private const val HASH_SET_ALLOCATION_THRESHOLD = 64
-
 @JvmInline
 public value class CollectionContainsAllCheck<E, C : Collection<E>>(
     public val elements: Collection<E>,
@@ -13,31 +11,18 @@ public value class CollectionContainsAllCheck<E, C : Collection<E>>(
         when {
             elements.isEmpty() -> true
             value.isEmpty() -> false
-            value is Set<*> -> value.containsAll(elements)
-            else -> {
-                val n = value.size
-                val m = elements.size
-
-                val lookup =
-                    if (m > 1 && n > HASH_SET_ALLOCATION_THRESHOLD / m) {
-                        value.toSet()
-                    } else {
-                        value
-                    }
-
-                elements.all { it in lookup }
-            }
+            else -> value.containsAll(elements)
         }
 }
 
 @Suppress("NOTHING_TO_INLINE")
 public inline fun <E, C : Collection<E>> CollectionContainsAllCheck(element: E): CollectionContainsAllCheck<E, C> =
     CollectionContainsAllCheck(
-        elements = setOf(element),
+        elements = listOf(element),
     )
 
 @Suppress("NOTHING_TO_INLINE")
 public inline fun <E, C : Collection<E>> CollectionContainsAllCheck(vararg elements: E): CollectionContainsAllCheck<E, C> =
     CollectionContainsAllCheck(
-        elements = setOf(elements = elements),
+        elements = elements.asList(),
     )
