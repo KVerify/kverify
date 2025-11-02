06ac20adb0d3ec214e93a70730a6a2bc6c8d6f94
tessoir <mrkekovich.official@gmail.com>
2025-11-02 13:18:14 +0300

    build(core): Remove "reflect" dependency

--- project/kverify/core/build.gradle.kts
+++ project/kverify/core/build.gradle.kts
@@ -2,11 +2,3 @@ plugins {
     `kverify-module`
     `kverify-module-publish`
 }
-
-kotlin {
-    sourceSets {
-        jvmMain.dependencies {
-            implementation(kotlin("reflect"))
-        }
-    }
-}
