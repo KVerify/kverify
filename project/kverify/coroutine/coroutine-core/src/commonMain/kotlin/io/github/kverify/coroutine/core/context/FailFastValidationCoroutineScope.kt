61435efc9993db89225d166f6b8e11847f85ae1c
tessoir <mrkekovich.official@gmail.com>
2025-11-13 08:39:11 +0300

    refactor(FailFastValidationCoroutineScope): Extend "FirstViolationValidationCoroutineScopeImpl"

--- project/kverify/coroutine/coroutine-core/src/commonMain/kotlin/io/github/kverify/coroutine/core/context/FailFastValidationCoroutineScope.kt
+++ project/kverify/coroutine/coroutine-core/src/commonMain/kotlin/io/github/kverify/coroutine/core/context/FailFastValidationCoroutineScope.kt
@@ -2,27 +2,18 @@ package io.github.kverify.coroutine.core.context
 
 import io.github.kverify.core.model.ValidationResult
 import io.github.kverify.core.violation.Violation
-import kotlinx.atomicfu.AtomicRef
-import kotlinx.atomicfu.atomic
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.supervisorScope
-import kotlin.coroutines.CoroutineContext
 
 @PublishedApi
 internal object AbortValidationException : RuntimeException()
 
 internal class FailFastValidationCoroutineScope(
     coroutineScope: CoroutineScope,
-) : ThrowingValidationCoroutineScope {
-    override val coroutineContext: CoroutineContext = coroutineScope.coroutineContext
-
-    private val _firstViolation: AtomicRef<Violation?> = atomic(null)
-
-    val firstViolation: Violation?
-        get() = _firstViolation.value
-
+) : FirstViolationValidationCoroutineScopeImpl(coroutineScope),
+    ThrowingValidationCoroutineScope {
     override fun onFailure(violation: Violation): Nothing {
-        _firstViolation.compareAndSet(null, violation)
+        super.onFailure(violation)
 
         throw AbortValidationException
     }
