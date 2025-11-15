ee82e7c372a7c7174bccd493bd4c450c29b71767
tessoir <mrkekovich.official@gmail.com>
2025-11-15 09:36:00 +0300

    feat(Rule.asContextual)

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/rule/ContextualRule.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/rule/ContextualRule.kt
@@ -1,6 +1,7 @@
 package io.github.kverify.core.rule
 
 import io.github.kverify.core.context.ValidationContext
+import kotlin.jvm.JvmInline
 
 public interface ContextualRule<in T> : Rule<T> {
     public fun ValidationContext.verify(value: T)
@@ -15,3 +16,14 @@ public inline fun <T> rule(crossinline verify: ValidationContext.(T) -> Unit): C
     object : ContextualRule<T> {
         override fun ValidationContext.verify(value: T): Unit = verify(value)
     }
+
+@Suppress("NOTHING_TO_INLINE")
+public inline fun <T> Rule<T>.asContextual(): ContextualRule<T> = ContextualRuleAdapter(this)
+
+@JvmInline
+@PublishedApi
+internal value class ContextualRuleAdapter<in T>(
+    private val originalRule: Rule<T>,
+) : ContextualRule<T> {
+    override fun ValidationContext.verify(value: T): Unit = originalRule.execute(this, value)
+}
