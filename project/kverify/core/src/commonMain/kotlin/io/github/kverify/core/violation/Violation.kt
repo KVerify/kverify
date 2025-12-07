f825b021efe0b7bcc4e9c0bc1cd51829dcc2a156
tessoir <mrkekovich.official@gmail.com>
2025-12-07 09:55:25 +0300

    docs(Violation.kt, ViolationReason.kt)

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/violation/Violation.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/violation/Violation.kt
@@ -1,5 +1,15 @@
 package io.github.kverify.core.violation
 
+/**
+ * Represents a validation failure with a description of why it failed.
+ *
+ * Implementations provide a [reason] describing what constraint was violated.
+ *
+ * @see ViolationReason
+ */
 public interface Violation {
+    /**
+     * A human-readable description of why the validation failed.
+     */
     public val reason: String
 }
--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/violation/ViolationReason.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/violation/ViolationReason.kt
@@ -2,13 +2,26 @@ package io.github.kverify.core.violation
 
 import kotlin.jvm.JvmInline
 
+/**
+ * A simple string-based implementation of [Violation].
+ *
+ * Wraps a string to provide a lightweight way to create violations.
+ *
+ * @property reason The human-readable description of the validation failure.
+ */
 @JvmInline
 public value class ViolationReason(
     override val reason: String,
 ) : Violation
 
+/**
+ * Wraps `this` [String] into a [ViolationReason].
+ */
 @Suppress("NOTHING_TO_INLINE")
 public inline fun String.asViolationReason(): ViolationReason = ViolationReason(this)
 
+/**
+ * Wraps given [reason] into a [ViolationReason].
+ */
 @Suppress("NOTHING_TO_INLINE")
 public inline fun violation(reason: String): ViolationReason = ViolationReason(reason)
