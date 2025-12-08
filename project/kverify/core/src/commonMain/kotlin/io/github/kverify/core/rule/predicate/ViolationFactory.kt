64b052e00914e048f629267d67aab030b7d3cd1f
tessoir <mrkekovich.official@gmail.com>
2025-12-08 09:40:38 +0300

    docs(ViolationFactory.kt)

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/rule/predicate/ViolationFactory.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/rule/predicate/ViolationFactory.kt
@@ -2,6 +2,16 @@ package io.github.kverify.core.rule.predicate
 
 import io.github.kverify.core.violation.Violation
 
+/**
+ * A factory that creates [Violation] instances for given values.
+ *
+ * Implementations define how to construct a violation based on the provided value.
+ *
+ * @see PredicateRule
+ */
 public fun interface ViolationFactory<in T> {
+    /**
+     * Creates a [Violation] for the given [value].
+     */
     public fun createViolation(value: T): Violation
 }
