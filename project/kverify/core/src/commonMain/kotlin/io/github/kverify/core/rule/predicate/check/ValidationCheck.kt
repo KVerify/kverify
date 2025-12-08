8928bd2d69155ceedb8cddc23aa9ef23d6aabed3
tessoir <mrkekovich.official@gmail.com>
2025-12-08 09:30:43 +0300

    feat(ValidationCheck.plus)

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/rule/predicate/check/ValidationCheck.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/rule/predicate/check/ValidationCheck.kt
@@ -1,5 +1,6 @@
 package io.github.kverify.core.rule.predicate.check
 
+import kotlin.collections.plus
 import kotlin.jvm.JvmInline
 
 /**
@@ -36,9 +37,24 @@ public fun interface ValidationCheck<in T> {
 public inline operator fun <T> ValidationCheck<T>.not(): ValidationCheck<T> = InvertedValidationCheck(this)
 
 /**
- * Wraps a [ValidationCheck] to invert its validation logic.
+ * Combines `this` check with another check using `AND` logic.
  *
- * @property originalCheck The check to invert
+ * If either check is a [ValidationCheckList], its checks are flattened into the result.
+ * Otherwise, both checks are combined into a new [ValidationCheckList].
+ *
+ * @param other The check to combine with
+ * @return A new [ValidationCheckList] that passes only if all checks pass
+ */
+public operator fun <T> ValidationCheck<T>.plus(other: ValidationCheck<T>): ValidationCheck<T> =
+    when {
+        this is ValidationCheckList && other is ValidationCheckList -> ValidationCheckList(this.checks + other.checks)
+        this is ValidationCheckList -> ValidationCheckList(this.checks + other)
+        else -> ValidationCheckList(this, other)
+    }
+
+/**
+ * Wraps [originalCheck] to invert its validation logic.
+ * @see ValidationCheck
  */
 @PublishedApi
 @JvmInline
