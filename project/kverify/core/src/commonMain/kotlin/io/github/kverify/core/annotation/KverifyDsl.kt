04a7206684378daf473ddb9b5bc6864c232151e3
tessoir <mrkekovich.official@gmail.com>
2025-12-09 09:58:29 +0300

    docs(KverifyDsl.kt)

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/annotation/KverifyDsl.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/annotation/KverifyDsl.kt
@@ -1,5 +1,12 @@
 package io.github.kverify.core.annotation
 
+/**
+ * Marks DSL elements in the Kverify API to prevent scope leakage.
+ *
+ * This annotation prevents implicit receiver mixing in nested DSL blocks.
+ *
+ * @see DslMarker
+ */
 @DslMarker
 @Target(AnnotationTarget.CLASS, AnnotationTarget.TYPEALIAS, AnnotationTarget.TYPE, AnnotationTarget.FUNCTION)
 public annotation class KverifyDsl
