1927ff9652d431ca6cc65a153005ef9e3fbd7b73
tessoir <mrkekovich.official@gmail.com>
2025-12-09 09:31:12 +0300

    docs(PredicateRule.kt)

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/rule/predicate/PredicateRule.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/rule/predicate/PredicateRule.kt
@@ -5,10 +5,20 @@ import io.github.kverify.core.context.failIfNot
 import io.github.kverify.core.rule.Rule
 import io.github.kverify.core.rule.predicate.check.ValidationCheck
 
+/**
+ * A [Rule] that validates values using [validationCheck] and reports violations created via [violationFactory].
+ *
+ * @see ValidationCheck
+ * @see ViolationFactory
+ */
 public class PredicateRule<in T>(
     public val validationCheck: ValidationCheck<T>,
     public val violationFactory: ViolationFactory<T>,
 ) : Rule<T> {
+    /**
+     * If [validationCheck] returns `false`, a [io.github.kverify.core.violation.Violation]
+     * is created using [violationFactory] and reported to the [context].
+     */
     override fun execute(
         context: ValidationContext,
         value: T,
