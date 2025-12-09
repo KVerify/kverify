b6c89b2f414e57c977ead9e0d62344c1fa09ee4c
tessoir <mrkekovich.official@gmail.com>
2025-12-09 09:41:31 +0300

    docs(Rule.kt, ScopedRule.kt)

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/rule/Rule.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/rule/Rule.kt
@@ -2,13 +2,32 @@ package io.github.kverify.core.rule
 
 import io.github.kverify.core.context.ValidationContext
 
+/**
+ * A unit of validation logic.
+ *
+ * @see RuleList
+ * @see ScopedRule
+ * @see io.github.kverify.core.rule.predicate.PredicateRule
+ */
 public interface Rule<in T> {
+    /**
+     * Defines the validation logic for the given [value] using the [context].
+     */
     public fun execute(
         context: ValidationContext,
         value: T,
     )
 }
 
+/**
+ * Combines `this` rule with another rule for sequential execution.
+ *
+ * If either rule is a [RuleList], its rules are flattened into the result.
+ * Otherwise, both rules are combined into a new [RuleList].
+ *
+ * @param other The [Rule] to combine with
+ * @return A new [RuleList] containing all rules
+ */
 public operator fun <T> Rule<T>.plus(other: Rule<T>): Rule<T> =
     when {
         this is RuleList && other is RuleList -> RuleList(this.rules + other.rules)
