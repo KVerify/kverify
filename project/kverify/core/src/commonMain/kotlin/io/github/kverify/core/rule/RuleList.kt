1815b49f1bf9a9bade57cbfe60fbd8cc9d506250
tessoir <mrkekovich.official@gmail.com>
2025-12-09 09:48:33 +0300

    docs(RuleList.kt)

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/rule/RuleList.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/rule/RuleList.kt
@@ -3,10 +3,16 @@ package io.github.kverify.core.rule
 import io.github.kverify.core.context.ValidationContext
 import kotlin.jvm.JvmInline
 
+/**
+ * A composite [Rule] that executes multiple [rules] sequentially.
+ */
 @JvmInline
 public value class RuleList<in T>(
     public val rules: List<Rule<T>>,
 ) : Rule<T> {
+    /**
+     * Executes all [rules] sequentially with the given [context] and [value].
+     */
     override fun execute(
         context: ValidationContext,
         value: T,
@@ -20,21 +26,47 @@ public value class RuleList<in T>(
     }
 }
 
+/**
+ * Creates an empty [RuleList].
+ */
 @Suppress("NOTHING_TO_INLINE")
 public inline fun <T> RuleList(): RuleList<T> = RuleList(emptyList())
 
+/**
+ * Creates a [RuleList] containing a single [rule].
+ */
 @Suppress("NOTHING_TO_INLINE")
 public inline fun <T> RuleList(rule: Rule<T>): RuleList<T> = RuleList(listOf(rule))
 
+/**
+ * Creates a [RuleList] containing the given [rules].
+ */
 @Suppress("NOTHING_TO_INLINE")
 public inline fun <T> RuleList(vararg rules: Rule<T>): RuleList<T> = RuleList(rules.asList())
 
+/**
+ * Combines `this` rule list with another rule.
+ *
+ * If [other] is a [RuleList], its rules are flattened into the result.
+ * Otherwise, [other] is appended as a single rule.
+ *
+ * @param other The [Rule] to add
+ * @return A new rule list containing all rules
+ */
 public operator fun <T> RuleList<T>.plus(other: Rule<T>): RuleList<T> =
     when (other) {
         is RuleList<T> -> RuleList(rules + other.rules)
         else -> RuleList(rules + other)
     }
 
+/**
+ * Combines `this` rule list with another rule list.
+ *
+ * The rules from both lists are flattened into a single list.
+ *
+ * @param other The [RuleList] to add
+ * @return A new [RuleList] containing all rules from both lists
+ */
 public operator fun <T> RuleList<T>.plus(other: RuleList<T>): RuleList<T> =
     RuleList(
         rules + other.rules,
