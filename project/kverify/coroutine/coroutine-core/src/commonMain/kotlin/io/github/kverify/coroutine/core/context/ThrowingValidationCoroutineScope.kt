e1b898753cc721ea59178dbdbe8c823f100e2461
tessoir <mrkekovich.official@gmail.com>
2025-12-15 09:31:49 +0300

    docs(coroutine-core)

--- project/kverify/coroutine/coroutine-core/src/commonMain/kotlin/io/github/kverify/coroutine/core/context/ThrowingValidationCoroutineScope.kt
+++ project/kverify/coroutine/coroutine-core/src/commonMain/kotlin/io/github/kverify/coroutine/core/context/ThrowingValidationCoroutineScope.kt
@@ -1,7 +1,14 @@
 package io.github.kverify.coroutine.core.context
 
 import io.github.kverify.core.context.ThrowingValidationContext
+import kotlinx.coroutines.CoroutineScope
 
+/**
+ * A [ThrowingValidationContext] that also implements [CoroutineScope].
+ *
+ * Enables fail-fast validation within coroutine contexts.
+ * [onFailure] must not return normally (it should throw an exception or terminate execution).
+ */
 public interface ThrowingValidationCoroutineScope :
     ValidationCoroutineScope,
     ThrowingValidationContext
--- project/kverify/coroutine/coroutine-core/src/commonMain/kotlin/io/github/kverify/coroutine/core/context/ValidationCoroutineScope.kt
+++ project/kverify/coroutine/coroutine-core/src/commonMain/kotlin/io/github/kverify/coroutine/core/context/ValidationCoroutineScope.kt
@@ -3,6 +3,15 @@ package io.github.kverify.coroutine.core.context
 import io.github.kverify.core.context.ValidationContext
 import kotlinx.coroutines.CoroutineScope
 
+/**
+ * A [ValidationContext] that also implements [CoroutineScope].
+ *
+ * Enables validation logic to be used within coroutine contexts,
+ * combining the validation DSL with coroutine capabilities.
+ *
+ * @see FirstViolationValidationCoroutineScope
+ * @see ThrowingValidationCoroutineScope
+ */
 public interface ValidationCoroutineScope :
     ValidationContext,
     CoroutineScope
