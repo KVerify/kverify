4848157470ef4c992bd6ecf8a92dafbef6a6f14e
tessoir <mrkekovich.official@gmail.com>
2025-12-23 12:45:47 +0300

    refactor: Remove class-based modules

--- project/settings.gradle.kts
+++ project/settings.gradle.kts
@@ -7,11 +7,9 @@ include(
     // rule-set
     ":kverify:rule-set",
     ":kverify:typed-violation-set",
-    ":kverify:class-based-violation-factory-set",
     // named
     ":kverify:named:named-value",
     ":kverify:named:named-rule-set",
-    ":kverify:named:named-class-based-violation-factory-set",
     // test
     ":test-kit",
 )
