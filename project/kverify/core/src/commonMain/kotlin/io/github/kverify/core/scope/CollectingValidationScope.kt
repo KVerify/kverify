ff3e82552e912668a35dbf10c1fac7272885c2aa
tessoir <mrkekovich.official@gmail.com>
2026-01-18 01:50:59 +0300

    feat(CollectingValidationScope.verifyWithCollecting)

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/scope/CollectingValidationScope.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/scope/CollectingValidationScope.kt
@@ -5,7 +5,6 @@ import io.github.kverify.core.context.ValidationContext
 import io.github.kverify.core.model.ValidationResult
 import io.github.kverify.core.rule.Rule
 import io.github.kverify.core.violation.Violation
-import kotlin.contracts.ExperimentalContracts
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
@@ -21,4 +20,50 @@ public open class CollectingValidationScope(
         ValidationResult(
             violationStorage.toList(),
         )
+}
+
+public inline fun verifyWithCollecting(
+    violationStorage: MutableCollection<Violation> = ArrayList(),
+    validationContext: ValidationContext = EmptyValidationContext,
+    block: CollectingValidationScope.() -> Unit,
+): ValidationResult {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+
+    return CollectingValidationScope(violationStorage, validationContext)
+        .apply(block)
+        .build()
+}
+
+public infix fun <T> T.verifyWithCollecting(rule: Rule<T>): ValidationResult {
+    val value = this
+
+    return verifyWithCollecting { value verifyWith rule }
+}
+
+public infix fun <T> T.verifyWithCollecting(rules: Iterable<Rule<T>>): ValidationResult {
+    val value = this
+
+    return verifyWithCollecting { value verifyWith rules }
+}
+
+public fun <T> T.verifyWithCollecting(
+    rules: Iterable<Rule<T>>,
+    violationStorage: MutableCollection<Violation> = ArrayList(),
+    validationContext: ValidationContext = EmptyValidationContext,
+): ValidationResult {
+    val value = this
+
+    return verifyWithCollecting(violationStorage, validationContext) { value verifyWith rules }
+}
+
+public fun <T> T.verifyWithCollecting(
+    vararg rules: Rule<T>,
+    violationStorage: MutableCollection<Violation> = ArrayList(),
+    validationContext: ValidationContext = EmptyValidationContext,
+): ValidationResult {
+    val value = this
+
+    return verifyWithCollecting(violationStorage, validationContext) { value.verifyWith(rules = rules) }
 }
