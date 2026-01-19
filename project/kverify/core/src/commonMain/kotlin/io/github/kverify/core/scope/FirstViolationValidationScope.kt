49890d0271408c0d76047a235ed4b64e8d725fe0
tessoir <mrkekovich.official@gmail.com>
2026-01-19 20:19:14 +0300

    feat(Collecting, FirstViolation, Throwing scopes): Add DSL functions, default implementations

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/scope/FirstViolationValidationScope.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/scope/FirstViolationValidationScope.kt
@@ -1,7 +1,11 @@
 package io.github.kverify.core.scope
 
+import io.github.kverify.core.context.EmptyValidationContext
+import io.github.kverify.core.context.ValidationContext
 import io.github.kverify.core.rule.Rule
 import io.github.kverify.core.violation.Violation
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 public interface FirstViolationValidationScope : ValidationScope {
     public val firstViolation: Violation?
@@ -38,3 +42,59 @@ public interface FirstViolationValidationScope : ValidationScope {
         return value
     }
 }
+
+public class DefaultFirstViolationValidationScope(
+    override val validationContext: ValidationContext = EmptyValidationContext,
+) : FirstViolationValidationScope {
+    override var firstViolation: Violation? = null
+        private set
+
+    override fun onFailure(violation: Violation) {
+        if (firstViolation == null) {
+            firstViolation = violation
+        }
+    }
+}
+
+public inline fun verifyWithFirstViolation(
+    validationContext: ValidationContext = EmptyValidationContext,
+    crossinline block: FirstViolationValidationScope.() -> Unit,
+): Violation? {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+
+    return DefaultFirstViolationValidationScope(validationContext)
+        .apply(block)
+        .firstViolation
+}
+
+public infix fun <T> T.passes(rule: Rule<T>): Boolean {
+    val value = this
+
+    return verifyWithFirstViolation {
+        value verifyWith rule
+    } == null
+}
+
+public infix fun <T> T.passes(rules: Iterable<Rule<T>>): Boolean {
+    val value = this
+
+    return verifyWithFirstViolation {
+        value verifyWith rules
+    } == null
+}
+
+public fun <T> T.passes(vararg rules: Rule<T>): Boolean {
+    val value = this
+
+    return verifyWithFirstViolation {
+        value.verifyWith(rules = rules)
+    } == null
+}
+
+public infix fun <T> T.notPasses(rule: Rule<T>): Boolean = !passes(rule)
+
+public infix fun <T> T.notPasses(rules: Iterable<Rule<T>>): Boolean = !passes(rules)
+
+public fun <T> T.notPasses(vararg rules: Rule<T>): Boolean = !passes(rules = rules)
--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/scope/ThrowingValidationScope.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/scope/ThrowingValidationScope.kt
@@ -1,7 +1,65 @@
 package io.github.kverify.core.scope
 
+import io.github.kverify.core.context.EmptyValidationContext
+import io.github.kverify.core.context.ValidationContext
+import io.github.kverify.core.exception.ThrowingValidationScopeException
 import io.github.kverify.core.violation.Violation
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
 public interface ThrowingValidationScope : ValidationScope {
     override fun onFailure(violation: Violation): Nothing
 }
+
+public inline fun ThrowingValidationScope.failIf(
+    condition: Boolean,
+    lazyViolation: () -> Violation,
+) {
+    contract {
+        returns() implies !condition
+    }
+
+    if (condition) {
+        val violation = lazyViolation()
+        onFailure(violation)
+    }
+}
+
+public inline fun ThrowingValidationScope.failIfNot(
+    condition: Boolean,
+    lazyViolation: () -> Violation,
+) {
+    contract {
+        returns() implies condition
+    }
+
+    failIf(!condition, lazyViolation)
+}
+
+public class DefaultThrowingValidationScope(
+    override val validationContext: ValidationContext = EmptyValidationContext,
+) : ThrowingValidationScope {
+    override fun onFailure(violation: Violation): Nothing = throw ThrowingValidationScopeException(violation)
+}
+
+public inline fun <T> verifyWithThrowing(
+    context: ValidationContext = EmptyValidationContext,
+    block: DefaultThrowingValidationScope.() -> T,
+): T {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+
+    return DefaultThrowingValidationScope(context).block()
+}
+
+public inline fun verifyWithFailFast(
+    context: ValidationContext = EmptyValidationContext,
+    block: DefaultThrowingValidationScope.() -> Unit,
+): Violation? =
+    try {
+        DefaultThrowingValidationScope(context).block()
+        null
+    } catch (e: ThrowingValidationScopeException) {
+        e.violation
+    }
