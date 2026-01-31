b841c38d5b7158d30740ba6321df6bb17528f679
tessoir <mrkekovich.official@gmail.com>
2026-01-31 09:25:29 +0300

    refactor(PredicateRule): Add "open" modifier to the class

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/rule/predicate/PredicateRule.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/rule/predicate/PredicateRule.kt
@@ -5,11 +5,11 @@ import io.github.kverify.core.rule.predicate.check.ValidationCheck
 import io.github.kverify.core.scope.ValidationScope
 import io.github.kverify.core.scope.failIf
 
-public class PredicateRule<in T>(
+public open class PredicateRule<in T>(
     public val validationCheck: ValidationCheck<T>,
     public val violationFactory: ViolationFactory<T>,
 ) : Rule<T> {
-    override fun execute(
+    final override fun execute(
         scope: ValidationScope,
         value: T,
     ) {
