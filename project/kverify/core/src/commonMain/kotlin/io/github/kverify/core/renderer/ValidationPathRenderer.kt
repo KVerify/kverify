5a50d3b57d25625806f3d3dbf1519fe58e4abf3a
tessoir <mrkekovich.official@gmail.com>
2026-01-31 09:24:16 +0300

    refactor(core.renderer): Remove unused package

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/renderer/ValidationPathRenderer.kt
+++
@@ -1,7 +0,0 @@
-package io.github.kverify.core.renderer
-
-import io.github.kverify.core.context.element.ValidationPathElement
-
-public fun interface ValidationPathRenderer {
-    public fun render(path: List<ValidationPathElement>): String?
-}
