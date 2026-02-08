0d3647be87e5d79154bf09ecb264879a9db578b3
tessoir <real.tessoir@gmail.com>
2026-02-08 15:01:02 +0300

    refactor(core.scope.decorator): Remove the package, ExtendedValidationScope and move Index and Property scopes into core.scope

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/scope/IndexValidationScope.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/scope/IndexValidationScope.kt
@@ -1,22 +1,21 @@
-package io.github.kverify.core.scope.decorator
+package io.github.kverify.core.scope
 
 import io.github.kverify.core.context.ValidationContext
 import io.github.kverify.core.context.element.ValidationPathElement
-import io.github.kverify.core.scope.ValidationScope
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
 public class IndexValidationScope<out T : ValidationScope>(
     validationContext: ValidationContext,
     index: Int,
-    public override val originalValidationScope: T,
-) : ValidationScopeDecorator<T> {
+    originalValidationScope: T,
+) : ValidationScope by originalValidationScope {
     override val validationContext: ValidationContext = validationContext + ValidationPathElement.index(index)
 }
 
 public inline fun <T : ValidationScope> T.index(
     index: Int,
-    block: IndexValidationScope<T>.() -> Unit,
+    block: IndexValidationScope<T>.() -> Unit = {},
 ): IndexValidationScope<T> {
     contract {
         callsInPlace(block, InvocationKind.EXACTLY_ONCE)
@@ -26,20 +25,5 @@ public inline fun <T : ValidationScope> T.index(
         validationContext = validationContext,
         index = index,
         originalValidationScope = this,
-    ).apply(block)
-}
-
-public inline fun <T : ValidationScope> ValidationScopeDecorator<T>.index(
-    index: Int,
-    block: IndexValidationScope<T>.() -> Unit,
-): IndexValidationScope<T> {
-    contract {
-        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
-    }
-
-    return IndexValidationScope(
-        validationContext = validationContext,
-        index = index,
-        originalValidationScope = originalValidationScope,
     ).apply(block)
 }
