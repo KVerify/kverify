ae447ebd30d5130f93f9c5f5e30f41d2bb14ac7d
tessoir <real.tessoir@gmail.com>
2026-02-10 21:20:31 +0300

    refactor(ValidationScope.verifyWith): Delegate to Verification interface

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/scope/ValidationScope.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/scope/ValidationScope.kt
@@ -3,65 +3,50 @@
 package io.github.kverify.core.scope
 
 import io.github.kverify.core.annotation.KverifyDsl
+import io.github.kverify.core.model.ListValidationContext
 import io.github.kverify.core.model.ValidationContext
+import io.github.kverify.core.model.ValidationPath
+import io.github.kverify.core.model.ValidationPathElement
 import io.github.kverify.core.rule.Rule
 import io.github.kverify.core.violation.Violation
+import kotlin.reflect.KProperty
+import kotlin.reflect.KProperty0
 
 @KverifyDsl
 public interface ValidationScope {
     public val validationContext: ValidationContext
 
     public fun onFailure(violation: Violation)
-
-    public infix fun <T> T.verifyWith(rule: Rule<T>): T {
-        val scope = this@ValidationScope
-        val value = this@verifyWith
-
-        rule.execute(
-            scope = scope,
-            value = value,
-        )
-
-        return value
-    }
-
-    public infix fun <T> T.verifyWith(rules: Iterable<Rule<T>>): T {
-        val value = this@verifyWith
-        val scope = this@ValidationScope
-
-        for (rule in rules) {
-            rule.execute(
-                scope = scope,
-                value = value,
-            )
-        }
-
-        return value
-    }
-
-    public fun <T> T.verifyWith(vararg rules: Rule<T>): T =
-        this.verifyWith(
-            rules = rules.asIterable(),
-        )
 }
 
-@Suppress("NOTHING_TO_INLINE")
-public inline fun <T> ValidationScope.verify(
-    value: T,
-    rule: Rule<T>,
-): T = value verifyWith rule
+public interface Verification<T> {
+    public fun enforce(rule: Rule<T>)
+}
 
-@Suppress("NOTHING_TO_INLINE")
-public inline fun <T> ValidationScope.verify(
-    value: T,
-    rules: Iterable<Rule<T>>,
-): T = value verifyWith rules
+@PublishedApi
+internal class ContextExtendedValidationScope<out T : ValidationScope>(
+    val originalValidationScope: T,
+    val additionalContext: ValidationContext,
+) : ValidationScope by originalValidationScope {
+    override val validationContext: ValidationContext
+        get() = originalValidationScope.validationContext + additionalContext
+}
 
-@Suppress("NOTHING_TO_INLINE")
-public inline fun <T> ValidationScope.verify(
-    value: T,
-    vararg rules: Rule<T>,
-): T = value.verifyWith(rules = rules)
+@PublishedApi
+internal class VerificationImpl<T>(
+    val value: T,
+    scope: ValidationScope,
+    path: ValidationPath = emptyList(),
+) : Verification<T> {
+    private val scope =
+        if (path.isNotEmpty()) {
+            scope + ListValidationContext(path)
+        } else {
+            scope
+        }
+
+    override fun enforce(rule: Rule<T>): Unit = rule.execute(scope, value)
+}
 
 public inline fun ValidationScope.failIf(
     condition: Boolean,
@@ -72,3 +57,61 @@ public inline fun ValidationScope.failIf(
         onFailure(violation)
     }
 }
+
+@Suppress("NOTHING_TO_INLINE")
+public inline fun <T> ValidationScope.verify(
+    path: ValidationPath,
+    value: T,
+): Verification<T> =
+    VerificationImpl(
+        value = value,
+        scope = this,
+        path = path,
+    )
+
+@Suppress("NOTHING_TO_INLINE")
+public inline fun <T> ValidationScope.verify(
+    vararg path: ValidationPathElement,
+    value: T,
+): Verification<T> =
+    VerificationImpl(
+        value = value,
+        scope = this,
+        path = path.asList(),
+    )
+
+@Suppress("NOTHING_TO_INLINE")
+public inline infix fun <T> ValidationScope.verify(value: T): Verification<T> =
+    VerificationImpl(
+        value = value,
+        scope = this,
+    )
+
+@Suppress("NOTHING_TO_INLINE")
+public inline infix fun <T> ValidationScope.verify(property: KProperty0<T>): Verification<T> =
+    VerificationImpl(
+        value = property.get(),
+        scope = this,
+        path = listOf(ValidationPathElement.Property(property.name)),
+    )
+
+@Suppress("NOTHING_TO_INLINE")
+public inline fun <T> ValidationScope.verify(
+    vararg path: KProperty<*>,
+    property: KProperty0<T>,
+): Verification<T> =
+    VerificationImpl(
+        value = property.get(),
+        scope = this,
+        path = path.map { ValidationPathElement.Property(it.name) },
+    )
+
+@Suppress("NOTHING_TO_INLINE")
+public inline infix fun <T> Verification<T>.with(rule: Rule<T>): Unit = enforce(rule)
+
+@Suppress("NOTHING_TO_INLINE")
+public inline operator fun ValidationScope.plus(validationContext: ValidationContext): ValidationScope =
+    ContextExtendedValidationScope(
+        originalValidationScope = this,
+        additionalContext = validationContext,
+    )
