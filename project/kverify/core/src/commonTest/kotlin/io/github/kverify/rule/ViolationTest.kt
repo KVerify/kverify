0f0d432d1d8d948ae5e53e3d60c10159bc4db5b0
tessoir <real.tessoir@gmail.com>
2026-02-23 13:06:34 +0300

    test(core)

---
+++ project/kverify/core/src/commonTest/kotlin/io/github/kverify/rule/ViolationTest.kt
@@ -0,0 +1,40 @@
+package io.github.kverify.rule
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+
+class ViolationTest {
+    @Test
+    fun violationReasonStoresReason() {
+        val violation = ViolationReason("something went wrong")
+
+        assertEquals("something went wrong", violation.reason)
+    }
+
+    @Test
+    fun violationReasonImplementsViolation() {
+        val violation = ViolationReason("error")
+
+        assertIs<Violation>(violation)
+    }
+
+    @Test
+    fun violationFactoryFunctionCreatesViolationReason() {
+        val violation = violation("error message")
+
+        assertIs<ViolationReason>(violation)
+        assertEquals("error message", violation.reason)
+    }
+
+    @Test
+    fun violationReasonValueClassEquality() {
+        val violation1 = ViolationReason("error")
+        val violation2 = ViolationReason("error")
+        val violation3 = ViolationReason("other")
+
+        assertEquals(violation1, violation2)
+        assertEquals(violation1.hashCode(), violation2.hashCode())
+        assertIs<Boolean>(violation1 != violation3)
+    }
+}
