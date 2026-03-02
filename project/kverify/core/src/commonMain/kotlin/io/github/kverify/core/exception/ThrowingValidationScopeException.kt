8b0c21b548a5275e4e4bca7bdc0a9e567c9a75ed
tessoir <real.tessoir@gmail.com>
2026-03-02 16:21:35 +0300

    refactor(ThrowingValidationScope): Remove the scope

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/exception/ThrowingValidationScopeException.kt
+++
@@ -1,7 +0,0 @@
-package io.github.kverify.core.exception
-
-import io.github.kverify.core.violation.Violation
-
-public class ThrowingValidationScopeException(
-    public val violation: Violation,
-) : ValidationException(listOf(violation))
