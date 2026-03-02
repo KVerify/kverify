b3ed86f31d92d7002e69b4b69030761d9db82d2d
tessoir <real.tessoir@gmail.com>
2026-03-02 19:55:12 +0300

    refactor(ViolationReason): Remove the class

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/violation/Violation.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/violation/Violation.kt
@@ -1,16 +1,5 @@
 package io.github.kverify.core.violation
 
-import kotlin.jvm.JvmInline
-
 public interface Violation {
     public val reason: String
 }
-
-@JvmInline
-@PublishedApi
-internal value class ViolationReason(
-    override val reason: String,
-) : Violation
-
-@Suppress("NOTHING_TO_INLINE")
-public inline fun violation(reason: String): Violation = ViolationReason(reason)
