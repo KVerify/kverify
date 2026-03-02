1dcb3093662d379365526c30aaae87693be2d675
tessoir <real.tessoir@gmail.com>
2026-03-02 16:25:35 +0300

    refactor(violations): Categorize by type and group by files

--- project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/violations/NotBlankViolation.kt
+++
@@ -1,8 +0,0 @@
-package io.github.kverify.violations
-
-import io.github.kverify.core.context.ValidationPathElement
-
-public data class NotBlankViolation(
-    override val validationPath: List<ValidationPathElement>,
-    override val reason: String,
-) : RuleSetViolation
