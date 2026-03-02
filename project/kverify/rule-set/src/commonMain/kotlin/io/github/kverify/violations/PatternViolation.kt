4a1e12fb152d39c5872d6ec84fb45b5248714319
tessoir <real.tessoir@gmail.com>
2026-03-02 16:22:39 +0300

    refactor(StringRules): Remove pattern rule

--- project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/violations/PatternViolation.kt
+++
@@ -1,9 +0,0 @@
-package io.github.kverify.violations
-
-import io.github.kverify.core.context.ValidationPathElement
-
-public data class PatternViolation(
-    val regex: Regex,
-    override val validationPath: List<ValidationPathElement>,
-    override val reason: String,
-) : RuleSetViolation
