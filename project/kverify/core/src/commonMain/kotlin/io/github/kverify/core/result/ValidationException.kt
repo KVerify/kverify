029eefe104bc2723e8936048146cd89dc40b4f66
tessoir <real.tessoir@gmail.com>
2026-03-05 22:50:26 +0300

    feat(ThrowingValidationScope, ViolationException)

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/result/ValidationException.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/result/ValidationException.kt
@@ -2,8 +2,12 @@ package io.github.kverify.core.result
 
 import io.github.kverify.core.violation.Violation
 
-public class ValidationException(
+public open class ValidationException(
     public val violations: List<Violation>,
 ) : RuntimeException() {
     override val message: String get() = "Validation failed with ${violations.size} violation(s)"
 }
+
+public class ViolationException(
+    public val violation: Violation,
+) : ValidationException(listOf(violation))
