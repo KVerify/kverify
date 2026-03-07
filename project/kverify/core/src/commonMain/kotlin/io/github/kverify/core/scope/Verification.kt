fd01c85885e106577ee7b7fb97ef93d204000fc8
tessoir <real.tessoir@gmail.com>
2026-03-07 09:35:51 +0300

    feat(Verification.each)

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/scope/Verification.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/scope/Verification.kt
@@ -1,5 +1,7 @@
 package io.github.kverify.core.scope
 
+import io.github.kverify.core.context.IndexPathElement
+
 public class Verification<out T>(
     public val value: T,
     public val scope: ValidationScope,
@@ -11,4 +13,18 @@ public fun <T : Any> Verification<T?>.takeIfNotNull(): Verification<T>? =
         this as Verification<T>
     } else {
         null
+    }
+
+public inline fun <T, I : Iterable<T>> Verification<I>.each(block: ValidationScope.(T) -> Unit): Verification<I> =
+    each { _, element ->
+        block(element)
+    }
+
+public inline fun <T, I : Iterable<T>> Verification<I>.each(block: ValidationScope.(Int, T) -> Unit): Verification<I> =
+    apply {
+        for ((idx, element) in value.withIndex()) {
+            val indexedScope = scope + IndexPathElement(idx)
+
+            indexedScope.block(idx, element)
+        }
     }
