ac270728c7a08d857136fd306e00246f2e642c11
tessoir <real.tessoir@gmail.com>
2026-03-09 13:23:43 +0300

    feat(ViolationImpl): Add a default implementation for "Violation" and a factory function

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/violation/Violation.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/violation/Violation.kt
@@ -3,3 +3,20 @@ package io.github.kverify.core.violation
 public interface Violation {
     public val reason: String
 }
+
+public fun violation(reason: String): Violation = ViolationImpl(reason)
+
+internal class ViolationImpl(
+    override val reason: String,
+) : Violation {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ViolationImpl) return false
+
+        return reason == other.reason
+    }
+
+    override fun hashCode(): Int = reason.hashCode()
+
+    override fun toString(): String = "Violation(reason=$reason)"
+}
