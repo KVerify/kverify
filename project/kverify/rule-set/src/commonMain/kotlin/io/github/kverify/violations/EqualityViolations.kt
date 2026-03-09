0b1e4935d7dc11f257efdc0f303b9e7502d20b58
tessoir <real.tessoir@gmail.com>
2026-03-09 13:22:06 +0300

    refactor: Remove "data" modifier from all classes and replace with manual "equals" and "hashCode"

--- project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/violations/EqualityViolations.kt
+++ project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/violations/EqualityViolations.kt
@@ -2,29 +2,112 @@ package io.github.kverify.violations
 
 import io.github.kverify.core.context.ValidationPathElement
 
-public data class EqualToViolation<T>(
-    val expected: T,
-    val actual: T,
+public class EqualToViolation<T>(
+    public val expected: T,
+    public val actual: T,
     override val validationPath: List<ValidationPathElement>,
     override val reason: String,
-) : RuleSetViolation
+) : RuleSetViolation {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is EqualToViolation<*>) return false
 
-public data class NotEqualToViolation<T>(
-    val forbidden: T,
-    override val validationPath: List<ValidationPathElement>,
-    override val reason: String,
-) : RuleSetViolation
+        return expected == other.expected &&
+            actual == other.actual &&
+            validationPath == other.validationPath &&
+            reason == other.reason
+    }
 
-public data class OneOfViolation<T>(
-    val allowed: Set<T>,
-    val actual: T,
-    override val validationPath: List<ValidationPathElement>,
-    override val reason: String,
-) : RuleSetViolation
+    override fun hashCode(): Int {
+        var result = expected.hashCode()
+        result = 31 * result + actual.hashCode()
+        result = 31 * result + validationPath.hashCode()
+        result = 31 * result + reason.hashCode()
 
-public data class NoneOfViolation<T>(
-    val forbidden: Set<T>,
-    val actual: T,
+        return result
+    }
+
+    override fun toString(): String = "EqualToViolation(expected=$expected, actual=$actual, validationPath=$validationPath, reason=$reason)"
+}
+
+public class NotEqualToViolation<T>(
+    public val forbidden: T,
     override val validationPath: List<ValidationPathElement>,
     override val reason: String,
-) : RuleSetViolation
+) : RuleSetViolation {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is NotEqualToViolation<*>) return false
+
+        return forbidden == other.forbidden &&
+            validationPath == other.validationPath &&
+            reason == other.reason
+    }
+
+    override fun hashCode(): Int {
+        var result = forbidden.hashCode()
+        result = 31 * result + validationPath.hashCode()
+        result = 31 * result + reason.hashCode()
+
+        return result
+    }
+
+    override fun toString(): String = "NotEqualToViolation(forbidden=$forbidden, validationPath=$validationPath, reason=$reason)"
+}
+
+public class OneOfViolation<T>(
+    public val allowed: Set<T>,
+    public val actual: T,
+    override val validationPath: List<ValidationPathElement>,
+    override val reason: String,
+) : RuleSetViolation {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is OneOfViolation<*>) return false
+
+        return allowed == other.allowed &&
+            actual == other.actual &&
+            validationPath == other.validationPath &&
+            reason == other.reason
+    }
+
+    override fun hashCode(): Int {
+        var result = allowed.hashCode()
+        result = 31 * result + actual.hashCode()
+        result = 31 * result + validationPath.hashCode()
+        result = 31 * result + reason.hashCode()
+
+        return result
+    }
+
+    override fun toString(): String = "OneOfViolation(allowed=$allowed, actual=$actual, validationPath=$validationPath, reason=$reason)"
+}
+
+public class NoneOfViolation<T>(
+    public val forbidden: Set<T>,
+    public val actual: T,
+    override val validationPath: List<ValidationPathElement>,
+    override val reason: String,
+) : RuleSetViolation {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is NoneOfViolation<*>) return false
+
+        return forbidden == other.forbidden &&
+            actual == other.actual &&
+            validationPath == other.validationPath &&
+            reason == other.reason
+    }
+
+    override fun hashCode(): Int {
+        var result = forbidden.hashCode()
+        result = 31 * result + actual.hashCode()
+        result = 31 * result + validationPath.hashCode()
+        result = 31 * result + reason.hashCode()
+
+        return result
+    }
+
+    override fun toString(): String =
+        "NoneOfViolation(forbidden=$forbidden, actual=$actual, validationPath=$validationPath, reason=$reason)"
+}
