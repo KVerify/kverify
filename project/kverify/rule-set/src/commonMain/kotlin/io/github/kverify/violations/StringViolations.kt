a3abe17143a784d22c1da8a6f2791fd0061e25ed
tessoir <real.tessoir@gmail.com>
2026-03-09 14:28:24 +0300

    refactor(violations): Remove RuleSetViolation and "equals" methods

--- project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/violations/StringViolations.kt
+++ project/kverify/rule-set/src/commonMain/kotlin/io/github/kverify/violations/StringViolations.kt
@@ -1,26 +1,12 @@
 package io.github.kverify.violations
 
 import io.github.kverify.core.context.ValidationPathElement
+import io.github.kverify.core.violation.PathAwareViolation
 
 public class NotBlankViolation(
     override val validationPath: List<ValidationPathElement>,
     override val reason: String,
-) : RuleSetViolation {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is NotBlankViolation) return false
-
-        return validationPath == other.validationPath &&
-            reason == other.reason
-    }
-
-    override fun hashCode(): Int {
-        var result = validationPath.hashCode()
-        result = 31 * result + reason.hashCode()
-
-        return result
-    }
-
+) : PathAwareViolation {
     override fun toString(): String = "NotBlankViolation(validationPath=$validationPath, reason=$reason)"
 }
 
@@ -29,26 +15,7 @@ public class MinLengthViolation(
     public val actualLength: Int,
     override val validationPath: List<ValidationPathElement>,
     override val reason: String,
-) : RuleSetViolation {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is MinLengthViolation) return false
-
-        return minLengthAllowed == other.minLengthAllowed &&
-            actualLength == other.actualLength &&
-            validationPath == other.validationPath &&
-            reason == other.reason
-    }
-
-    override fun hashCode(): Int {
-        var result = minLengthAllowed.hashCode()
-        result = 31 * result + actualLength.hashCode()
-        result = 31 * result + validationPath.hashCode()
-        result = 31 * result + reason.hashCode()
-
-        return result
-    }
-
+) : PathAwareViolation {
     override fun toString(): String =
         "MinLengthViolation(minLengthAllowed=$minLengthAllowed, actualLength=$actualLength, validationPath=$validationPath, reason=$reason)"
 }
@@ -58,26 +25,7 @@ public class MaxLengthViolation(
     public val actualLength: Int,
     override val validationPath: List<ValidationPathElement>,
     override val reason: String,
-) : RuleSetViolation {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is MaxLengthViolation) return false
-
-        return maxLengthAllowed == other.maxLengthAllowed &&
-            actualLength == other.actualLength &&
-            validationPath == other.validationPath &&
-            reason == other.reason
-    }
-
-    override fun hashCode(): Int {
-        var result = maxLengthAllowed.hashCode()
-        result = 31 * result + actualLength.hashCode()
-        result = 31 * result + validationPath.hashCode()
-        result = 31 * result + reason.hashCode()
-
-        return result
-    }
-
+) : PathAwareViolation {
     override fun toString(): String =
         "MaxLengthViolation(maxLengthAllowed=$maxLengthAllowed, actualLength=$actualLength, validationPath=$validationPath, reason=$reason)"
 }
@@ -87,26 +35,7 @@ public class ExactLengthViolation(
     public val actualLength: Int,
     override val validationPath: List<ValidationPathElement>,
     override val reason: String,
-) : RuleSetViolation {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is ExactLengthViolation) return false
-
-        return expectedLength == other.expectedLength &&
-            actualLength == other.actualLength &&
-            validationPath == other.validationPath &&
-            reason == other.reason
-    }
-
-    override fun hashCode(): Int {
-        var result = expectedLength.hashCode()
-        result = 31 * result + actualLength.hashCode()
-        result = 31 * result + validationPath.hashCode()
-        result = 31 * result + reason.hashCode()
-
-        return result
-    }
-
+) : PathAwareViolation {
     override fun toString(): String =
         "ExactLengthViolation(expectedLength=$expectedLength, actualLength=$actualLength, validationPath=$validationPath, reason=$reason)"
 }
@@ -117,28 +46,7 @@ public class LengthRangeViolation(
     public val actualLength: Int,
     override val validationPath: List<ValidationPathElement>,
     override val reason: String,
-) : RuleSetViolation {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other !is LengthRangeViolation) return false
-
-        return minLengthAllowed == other.minLengthAllowed &&
-            maxLengthAllowed == other.maxLengthAllowed &&
-            actualLength == other.actualLength &&
-            validationPath == other.validationPath &&
-            reason == other.reason
-    }
-
-    override fun hashCode(): Int {
-        var result = minLengthAllowed.hashCode()
-        result = 31 * result + maxLengthAllowed.hashCode()
-        result = 31 * result + actualLength.hashCode()
-        result = 31 * result + validationPath.hashCode()
-        result = 31 * result + reason.hashCode()
-
-        return result
-    }
-
+) : PathAwareViolation {
     override fun toString(): String =
         "LengthRangeViolation(minLengthAllowed=$minLengthAllowed, maxLengthAllowed=$maxLengthAllowed, actualLength=$actualLength, validationPath=$validationPath, reason=$reason)"
 }
