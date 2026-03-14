d1d4dd0f8e2c09dba28ce9740c35a5d52e91b7d4
tessoir <real.tessoir@gmail.com>
2026-03-14 10:34:54 +0300

    refactor(ValidationContext): Remove "fold" method and extend "Iterable" interface

--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/context/CombinedContext.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/context/CombinedContext.kt
@@ -4,24 +4,20 @@ internal class CombinedContext(
     private val left: ValidationContext,
     private val right: ValidationContext.Element,
 ) : ValidationContext {
-    override fun <R> fold(
-        initial: R,
-        operation: (R, ValidationContext.Element) -> R,
-    ): R {
-        var cur: ValidationContext = this
-        val rights = ArrayList<ValidationContext.Element>()
+    override fun iterator(): Iterator<ValidationContext.Element> =
+        iterator {
+            var cur: ValidationContext = this@CombinedContext
+            val rights = ArrayList<ValidationContext.Element>()
 
-        while (cur is CombinedContext) {
-            rights.add(cur.right)
-            cur = cur.left
+            while (cur is CombinedContext) {
+                rights.add(cur.right)
+                cur = cur.left
+            }
+
+            yieldAll(cur)
+
+            for (i in rights.indices.reversed()) {
+                yield(rights[i])
+            }
         }
-
-        var result = cur.fold(initial, operation)
-
-        for (i in rights.lastIndex downTo 0) {
-            result = operation(result, rights[i])
-        }
-
-        return result
-    }
 }
--- project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/context/EmptyValidationContext.kt
+++ project/kverify/core/src/commonMain/kotlin/io/github/kverify/core/context/EmptyValidationContext.kt
@@ -1,10 +1,7 @@
 package io.github.kverify.core.context
 
 public object EmptyValidationContext : ValidationContext {
-    override fun <R> fold(
-        initial: R,
-        operation: (R, ValidationContext.Element) -> R,
-    ): R = initial
-
     override fun plus(other: ValidationContext): ValidationContext = other
+
+    override fun iterator(): Iterator<ValidationContext.Element> = emptyList<ValidationContext.Element>().iterator()
 }
