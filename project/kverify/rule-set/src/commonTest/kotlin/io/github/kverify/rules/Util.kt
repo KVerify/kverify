4fe2388c9cdabddc43694246ce80f445dc6c4fc2
tessoir <real.tessoir@gmail.com>
2026-03-18 00:00:45 +0300

    test(Util.kt): Change "collecting" parameter type to generic "ValidationContext"

--- project/kverify/rule-set/src/commonTest/kotlin/io/github/kverify/rules/Util.kt
+++ project/kverify/rule-set/src/commonTest/kotlin/io/github/kverify/rules/Util.kt
@@ -1,7 +1,7 @@
 package io.github.kverify.rules
 
 import io.github.kverify.core.context.EmptyValidationContext
-import io.github.kverify.core.context.NamePathElement
+import io.github.kverify.core.context.ValidationContext
 import io.github.kverify.core.scope.CollectingValidationScope
 import io.github.kverify.core.scope.ThrowingValidationScope
 import io.github.kverify.core.scope.Verification
@@ -9,10 +9,9 @@ import io.github.kverify.core.violation.Violation
 
 fun <T> collecting(
     value: T,
-    pathElement: NamePathElement? = null,
+    context: ValidationContext = EmptyValidationContext,
 ): Pair<MutableList<Violation>, Verification<T>> {
     val storage = mutableListOf<Violation>()
-    val context = pathElement ?: EmptyValidationContext
     val scope = CollectingValidationScope(storage, context)
     return storage to Verification(value, scope)
 }
